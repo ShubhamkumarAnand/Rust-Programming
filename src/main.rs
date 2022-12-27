896f7d83e827addac0149b0a867690f94ba1709f
imskanand <the.anand3099@gmail.com>
2022-12-27 20:29:27 +0530

    Random Program for a int Number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main(){
-  println!("Hello 🗺")
+fn main() {
+    let mut x: i32 = 6; // Mutable Variable 
+    print!("{x}");
+    while x != 1 {
+        if x % 2 == 0 {
+            x /= 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!("->{x}");
+    }
+    println!();
 }
