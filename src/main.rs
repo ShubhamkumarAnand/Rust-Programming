71823b952f0198e9c1fcf2dc1bfab301f1049e30
imskanand <the.anand3099@gmail.com>
2022-12-27 20:08:53 +0530

    Starting Again!!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-fn main() {
-    let x = 5;
-    {
-      let x = 99;
-      print!("x: {}\n", x);
-    }
-    print!("x: {}\n", x);
-
-    let mut y = 100;
-    let y = 10;
-    print!("y: {}\n", y);
-
-    let meme = "Hari OM!!";
-    // let meme = make_image(meme);
-    print!("meme: {}\n", meme);
+fn main(){
+  println!("Hello 🗺")
 }
